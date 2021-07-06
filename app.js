c430b8414e6c6030e8eac530f6b328ef7094f87c
nicolas-moreira <nicolastoto2@gmail.com>
2021-07-06 05:03:48 +0200

    Startup express, and added a route

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+const app = express();
+const port = 3000;
+
+const server = app.listen(port,() => console.log('Server listening on port',port));
+
+app.get('/', (req ,res ,next) => {
+    res.status(200).send('Welcome');
+});
