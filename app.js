6f9d0efa6932592f7284665754534c0a8801f830
nicolas-moreira <nicolastoto2@gmail.com>
2021-07-06 05:13:47 +0200

    Added view folder, home.pug wwith simple html code, and set express views folder

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 const server = app.listen(port,() => console.log('Server listening on port',port));
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.get('/', (req ,res ,next) => {
     res.status(200).send('Welcome');
 });
